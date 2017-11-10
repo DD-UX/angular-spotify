d9f1cb2d626042213cc848704ea6b39bac747786
Diego Díaz <diego@diegodiazweb.com>
2017-11-10 19:57:08 -0300

    Branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { CommonsModule } from './commons/commons.module';
 import { ViewsModule } from './views/views.module';
 import { RouterConfig } from './app.router.config';
 import { RouterModule } from '@angular/router';
-
 
 @NgModule({
   declarations: [
