36e10609f153935b9f213e011e419af8ee1229be
Diego Díaz <diego@diegodiazweb.com>
2017-11-10 15:33:39 -0300

    Making a markup list of elements (just a basic prototype)

--- src/app/views/favorites/favorites.component.ts
+++ src/app/views/favorites/favorites.component.ts
@@ -2,12 +2,48 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'fdv-favorites',
-  template: `
-    <p>
-      favorites works!
-    </p>
-  `,
-  styles: [],
+    template: `
+      <div class="container">
+        <ul class="list-unstyled favorites__list row justify-content-around">
+          <li class="media mt-4 d-flex flex-column col-6 align-items-center col-md-3 col-xl-3">
+            <img class="mb-3 rounded-circle" src="https://avatars2.githubusercontent.com/u/9665653?s=460&v=4" alt="Placeholder">
+            <div class="media-body">
+              <h5 class="mb-2">List-based media object</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </li>
+          <li class="media mt-4 d-flex flex-column col-6 align-items-center col-md-3 col-xl-3">
+            <img class="mb-3 rounded-circle" src="https://avatars2.githubusercontent.com/u/9665653?s=460&v=4" alt="Placeholder">
+            <div class="media-body">
+              <h5 class="mb-2">List-based media object</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </li>
+          <li class="media mt-4 d-flex flex-column col-6 align-items-center col-md-3 col-xl-3">
+            <img class="mb-3 rounded-circle" src="https://avatars2.githubusercontent.com/u/9665653?s=460&v=4" alt="Placeholder">
+            <div class="media-body">
+              <h5 class="mb-2">List-based media object</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </li>
+          <li class="media mt-4 d-flex flex-column col-6 align-items-center col-md-3 col-xl-3">
+            <img class="mb-3 rounded-circle" src="https://avatars2.githubusercontent.com/u/9665653?s=460&v=4" alt="Placeholder">
+            <div class="media-body">
+              <h5 class="mb-2">List-based media object</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </li>
+          <li class="media mt-4 d-flex flex-column col-6 align-items-center col-md-3 col-xl-3">
+            <img class="mb-3 rounded-circle" src="https://avatars2.githubusercontent.com/u/9665653?s=460&v=4" alt="Placeholder">
+            <div class="media-body">
+              <h5 class="mb-2">List-based media object</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+          </li>
+        </ul>
+      </div>
+    `,
+    styleUrls: ['./_favorites.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class FavoritesComponent implements OnInit {
